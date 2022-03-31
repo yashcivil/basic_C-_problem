4e13fbff64b2861af2e787b11998b79a3018f591
yashcivil <102730520+yashcivil@users.noreply.github.com>
2022-03-31 19:37:21 +0530

    Tutorial 10
    sizeof

---
+++ tut10.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+    //   int a,b,c;
+    // cout<<"Enter the value of a :"<< endl;
+    // cin>>a;
+    // cout<<"Enter the value of b:"<< endl;
+    // cin>>b;
+    // c=a+b;
+    // cout<<"The sum is "<<c<<endl;
+    // cout<<"The globle c is "<<::c
+
+    float d=23.4f;
+    long double e=23.4;
+    cout<<"The size of 23.4 is " <<sizeof(23.4)<<endl;
+    cout<<"The size of 23.4f is " <<sizeof(23.4f)<<endl;
+    cout<<"The size of 23.4F is " <<sizeof(23.4F)<<endl;
+    cout<<"The size of 23.4l is " <<sizeof(23.4l)<<endl; 
+    cout<<"The size of 23.4L is " <<sizeof(23.4L)<<endl;
+
+    cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e;
+    return 0;
+ }
