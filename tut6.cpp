3aa5520bc28e7e7875d67f80c9dd7bdea98a0401
yashcivil <102730520+yashcivil@users.noreply.github.com>
2022-03-31 16:41:56 +0530

    Tutorial 6
    Sum of two numbers

---
+++ tut6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+     int num1;
+     int num2;
+     cout<<"Enter the value of num1";
+    cin>>num1;
+    cout<<"Enter the value of num2";
+    cin>>num2;
+    cout<<"The sum is"<<num1+num2;
+    return 0;
+ }
