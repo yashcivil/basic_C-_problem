ca3c7fff7e6f97629b6c2ac51c216c059ae378a9
yashcivil <102730520+yashcivil@users.noreply.github.com>
2022-03-31 16:39:02 +0530

    Tutorial 5
    Local and global variable

---
+++ tut5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int glo = 6;
+void sum(){
+    int a;
+    cout<<glo;
+}
+int main()
+{
+    int glo=9;
+   glo=50;
+    int a=3;
+    int b=5;
+    float c=2.5;
+    char d='u';
+    bool is_true = false;
+   sum();
+   cout<<glo<< is_true;
+   
+    // cout<<" This is tutorial 4. \nHere the value of a is " <<a<< " \n The value of b is "<< b;
+    // cout<<"\nThe value of c is "<<c;
+    // cout<<"\nThe value of d is "<<d;
+    return 0;
+}
