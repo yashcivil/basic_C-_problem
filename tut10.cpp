075f79518a13fe37f0ae75828b5f356cd7de3121
yashcivil <102730520+yashcivil@users.noreply.github.com>
2022-04-01 16:10:10 +0530

    Tutorial 10
    Build in data types, Float, double and long double Literals, reference variables, typecasting

--- tut10.cpp
+++ tut10.cpp
@@ -2,6 +2,7 @@
 using namespace std;
  int main()
  {
+    //  ***********Build in data types************
     //   int a,b,c;
     // cout<<"Enter the value of a :"<< endl;
     // cin>>a;
@@ -11,14 +12,36 @@ using namespace std;
     // cout<<"The sum is "<<c<<endl;
     // cout<<"The globle c is "<<::c
 
-    float d=23.4f;
-    long double e=23.4;
-    cout<<"The size of 23.4 is " <<sizeof(23.4)<<endl;
-    cout<<"The size of 23.4f is " <<sizeof(23.4f)<<endl;
-    cout<<"The size of 23.4F is " <<sizeof(23.4F)<<endl;
-    cout<<"The size of 23.4l is " <<sizeof(23.4l)<<endl; 
-    cout<<"The size of 23.4L is " <<sizeof(23.4L)<<endl;
+//  ***********Float, double and long double Literals************
+    // float d=23.4f;
+    // long double e=23.4;
+    // cout<<"The size of 23.4 is " <<sizeof(23.4)<<endl;
+    // cout<<"The size of 23.4f is " <<sizeof(23.4f)<<endl;
+    // cout<<"The size of 23.4F is " <<sizeof(23.4F)<<endl;
+    // cout<<"The size of 23.4l is " <<sizeof(23.4l)<<endl; 
+    // cout<<"The size of 23.4L is " <<sizeof(23.4L)<<endl;
+ 
+    // cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e;
+    
+    // **********reference variables***********
+    // Rohan Das---> Monty----> Rohu----> Dangerous Player
+    // float x = 225;
+    // float & y = x;
+    // cout<<x<<endl;
+    // cout<<y<<endl;
 
-    cout<<"The value of d is "<<d<<endl<<"The value of e is "<<e;
+    //  ***********Typecasting************
+       int a= 35;
+       float b=35.34;
+       cout<<"The value of a is "<<(float)a<<endl;
+       cout<<"The value of a is "<<float(a)<<endl;
+
+       cout<<"The value of b is "<<(int)b<<endl; 
+       cout<<"The value of b is "<<int(b)<<endl;
+        int c=int (b);
+       cout<<"The expression is "<<a+b<<endl;
+       cout<<"The expression is "<< a+ int(b)<<endl;
+       cout<<"The expression is "<<(int)b<<endl;
     return 0;
  }
+ 
