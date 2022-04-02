d2b87335af7f9534d2ac4e0d095abe6a637b0c5c
yashcivil <102730520+yashcivil@users.noreply.github.com>
2022-04-02 19:14:41 +0530

    Tutorial 17
    Array with while and do while loop

---
+++ tut17.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+      int marks[]={10,20,30,40};
+      int i=0;
+    //   while(i<=3)
+    //   {
+    //       cout<<marks[i]<<endl;
+    //       i++;
+    //   }
+    do
+    {
+        cout<<marks[i]<<endl;
+        i++;
+    }while(i<=3);
+    
+    return 0;
+}
