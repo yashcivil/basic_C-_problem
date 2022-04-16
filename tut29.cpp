ccc7c07bde0b681fd003e6a71fc405cca3036abf
yashcivil <102730520+yashcivil@users.noreply.github.com>
2022-04-16 19:04:48 +0530

    Tutorial 29
    Friend function & member function

---
+++ tut29.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+// Forword declaration
+class Complex;
+
+class Calculator
+{
+public:
+    int add(int a, int b)
+    {
+        return (a + b);
+    }
+    int sumRealComplex(Complex, Complex);
+    int sumCompComplex(Complex, Complex);
+};
+
+class Complex
+{
+    int a, b;
+    // individually declaring function as ariend
+    friend int Calculator ::sumRealComplex(Complex, Complex);
+    friend int Calculator ::sumCompComplex(Complex o1, Complex o2);
+    // Ailiter--> Declaring the entire calculator as freind
+    friend class calculator;
+
+public:
+    void setNumber(int n1, int n2)
+    {
+        a = n1;
+        b = n2;
+    }
+
+    void printNumber()
+    {
+        cout << "Your complex number is " << a << " + " << b << "i" << endl;
+    }
+};
+
+int Calculator ::sumRealComplex(Complex o1, Complex o2)
+{
+    return (o1.a + o2.a);
+}
+int Calculator ::sumCompComplex(Complex o1, Complex o2)
+{
+    return (o1.b + o2.b);
+}
+int main()
+{
+    Complex o1, o2;
+    o1.setNumber(1, 4);
+    o2.setNumber(5, 7);
+    Calculator calc;
+    int res = calc.sumRealComplex(o1, o2);
+    cout << "the sum of real part of o1 and o2 is " << res << endl;
+    int resc = calc.sumCompComplex(o1, o2);
+    cout << "the sum of complex part of o1 and o2 is " << resc << endl;
+    return 0;
+}
